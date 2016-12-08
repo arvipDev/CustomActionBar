854e8e146edf9d004a1fa7a6f77db5ed85d47ad2
arvipDev <arvind.purushotham@gmail.com>
2016-12-07 20:28:39 -0700

    1.0.3_customAB - Completed...

--- app/src/main/java/com/purushotham/arvind/customactionbar/CustomActionBar.java
+++ app/src/main/java/com/purushotham/arvind/customactionbar/CustomActionBar.java
@@ -1,7 +1,10 @@
 package com.purushotham.arvind.customactionbar;
 
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import services.CustomActionBarSvcImpl;
 
 public class CustomActionBar extends AppCompatActivity {
 
@@ -9,5 +12,10 @@ public class CustomActionBar extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_custom_action_bar);
+
+        ActionBar actionBar = getSupportActionBar();
+        CustomActionBarSvcImpl service = new CustomActionBarSvcImpl(this, actionBar, "Custom Action Bar");
+        service.setting();
+
     }
 }
